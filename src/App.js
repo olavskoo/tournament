35170b70b9343701ea7340b36365afab7823235b
qkrooz <46641568+qkrooz@users.noreply.github.com>
2020-07-03 05:12:39 -0600

    CAMBIOS HEADER
    Creacion de textos
    Creacion de Nav BAR
    Sistema de lenguaje.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// Other
+import texts from "./_resources/texts.json";
+import "./App.css";
+import "antd/dist/antd.css";
+import Header from "./_components/NavBar";
+import Footer from "./_components/Footer";
+// Ant Design
+import { Layout } from "antd";
+const { Content } = Layout;
 
 function App() {
+  const [language, setLanguage] = useState("es");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ height: "100%", backgroundColor: "#292d31" }}>
+      <Header lang={language} setLanguage={setLanguage} texts={texts} />
+      <Content></Content>
+      <Footer />
+    </Layout>
   );
 }
 
---
+++ src/_components/NavBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import * as Icon from "react-feather";
+import { Menu, Dropdown } from "antd";
+const NavBar = ({ lang, setLanguage, texts }) => {
+  let isLogged = false;
+  const langMenu = (
+    <Menu>
+      {lang == "es" ? (
+        <Menu.Item>
+          <a onClick={() => setLanguage("en")}>Inglés</a>
+        </Menu.Item>
+      ) : (
+        <Menu.Item>
+          {" "}
+          <a onClick={() => setLanguage("es")}>Spanish</a>
+        </Menu.Item>
+      )}
+    </Menu>
+  );
+  return (
+    <div className="header">
+      <nav>
+        <div className="logo" />
+        <a>{texts[lang].nav[1]}</a>
+        <a>{texts[lang].nav[2]}</a>
+        <a>{texts[lang].nav[3]}</a>
+        <a>{texts[lang].nav[4]}</a>
+      </nav>
+      <div className="controls">
+        <Dropdown overlay={langMenu} placement="bottomLeft" arrow>
+          <a className="control">
+            <p class="control-stamp">{lang}</p>
+            <Icon.Globe color={"#e2e2e2"} />
+          </a>
+        </Dropdown>
+        <a className="control">
+          <Icon.User color={"#e2e2e2"} />
+          {isLogged ? null : (
+            <p class="control-stamp">{texts[lang].login_stamp}</p>
+          )}
+        </a>
+      </div>
+    </div>
+  );
+};
+export default NavBar;
