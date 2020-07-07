64ba74792afda22c163fa3dbf643e2c9019371c4
qkrooz <46641568+qkrooz@users.noreply.github.com>
2020-07-07 02:44:11 -0600

    Changes for HOME

--- src/_screens/Home.js
+++ src/_screens/Home.js
@@ -1,5 +1,54 @@
 import React from "react";
+import "./Home.css";
+import { Row, Col } from "antd";
+// Others
+import Team1 from "../_img/team1.png";
+import Team2 from "../_img/team2.png";
+const Event = () => {
+  return (
+    <div className="event-container">
+      <p className="timeStamp">8:00 AM</p>
+      <div className="teams-container" style={{ paddingBottom: 0 }}>
+        <img src={Team1} alt="team-logo" className="teamImage" />
+        <p className="teamName">HLG</p>
+      </div>
+      <div className="teams-container">
+        <img src={Team2} alt="team-logo" className="teamImage" />
+        <p className="teamName">III</p>
+      </div>
+    </div>
+  );
+};
+const Schedule = () => {
+  return (
+    <Col span={3} style={{ maxHeight: "100%", overflow: "auto" }}>
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+    </Col>
+  );
+};
+const Stream = () => {
+  return (
+    <Row>
+      <h1>Streaming!</h1>
+    </Row>
+  );
+};
 const Home = () => {
-  return <h1>Home</h1>;
+  let isStreaming = true;
+  let isSchedule = true;
+  return (
+    <Row style={{ height: "100%" }}>
+      {isSchedule ? <Schedule /> : null}
+      <Col style={{ backgroundColor: "#ffffff", flexGrow: 1 }}>
+        {isStreaming ? <Stream /> : null}
+        <Row></Row>
+      </Col>
+    </Row>
+  );
 };
 export default Home;
