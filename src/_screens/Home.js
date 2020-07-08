99391054733e98ac00ef8513a7efb74d606e375a
qkrooz <46641568+qkrooz@users.noreply.github.com>
2020-07-08 01:26:29 -0600

    Update Home.js
    Tipos de StandardPost

--- src/_screens/Home.js
+++ src/_screens/Home.js
@@ -4,20 +4,20 @@ import "./Posts.css";
 import { Row, Col } from "antd";
 import ReactTwitchEmbedVideo from "react-twitch-embed-video";
 // fontawesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 import * as Icon from "react-feather";
 // Others
 import PageEnd from "../_components/PageEnd";
 import Team1 from "../_img/team1.png";
 import Team2 from "../_img/team2.png";
-import ExampleImg from '../_img/example1.jpg';
-import ExampleImg2 from '../_img/example2.jpg';
-import ExampleImg3 from '../_img/example3.jpg';
-import AdImage from '../_img/ad.jpg';
-import AdImage2 from '../_img/ad2.jpg';
-import BannerImg from '../_img/banner.png';
-import FullWidthImage from '../_img/fullwidth.jpg';
+import ExampleImg from "../_img/example1.jpg";
+import ExampleImg2 from "../_img/example2.jpg";
+import ExampleImg3 from "../_img/example3.jpg";
+import AdImage from "../_img/ad.jpg";
+import AdImage2 from "../_img/ad2.jpg";
+import BannerImg from "../_img/banner.png";
+import FullWidthImage from "../_img/fullwidth.jpg";
 
 const Event = () => {
   return (
@@ -59,7 +59,7 @@ const Stream = () => {
     </Row>
   );
 };
-const StandardPost = ({ img, content }) => {
+const StandardPost = ({ img, content, type }) => {
   return (
     <Col span={12}>
       <a>
@@ -68,11 +68,15 @@ const StandardPost = ({ img, content }) => {
           <div className="image">
             <img src={img} alt="post-image" className="post-image" />
           </div>
-          <p className="post-type">ARTICULO</p>
+          <p className="post-type">{type}</p>
           <div className="postFooter">
             <p className="title">{content}</p>
             <div className="posttimeago">
-              <FontAwesomeIcon icon={faClock} color={'white'} style={{ marginRight: '0.5em' }} />
+              <FontAwesomeIcon
+                icon={faClock}
+                color={"white"}
+                style={{ marginRight: "0.5em" }}
+              />
               <p className="date">01/01/2000 5:45:00 PM</p>
             </div>
           </div>
@@ -80,20 +84,22 @@ const StandardPost = ({ img, content }) => {
       </a>
     </Col>
   );
-}
+};
 const FullWidthPost = () => {
-  return (<Col span={24}>
-    <div className="fullWidthPostContainer">
-      <a>
-        <img src={FullWidthImage} alt="fullwidth-banner" />
-        <div className="fw-info">
-          <p className="fw-title">CS: GO</p>
-          <p className="fw-subtitle">¡Comienza tu viaje a la cima!</p>
-        </div>
-      </a>
-    </div>
-  </Col>)
-}
+  return (
+    <Col span={24}>
+      <div className="fullWidthPostContainer">
+        <a>
+          <img src={FullWidthImage} alt="fullwidth-banner" />
+          <div className="fw-info">
+            <p className="fw-title">CS: GO</p>
+            <p className="fw-subtitle">¡Comienza tu viaje a la cima!</p>
+          </div>
+        </a>
+      </div>
+    </Col>
+  );
+};
 const BannerPromo = () => {
   return (
     <Col span={24}>
@@ -103,15 +109,15 @@ const BannerPromo = () => {
         </a>
       </div>
     </Col>
-  )
-}
+  );
+};
 const Ad = ({ img }) => {
   return (
     <div className="adContainer">
       <img src={img} alt="ad-image" />
     </div>
-  )
-}
+  );
+};
 const Home = ({ isLive, lang }) => {
   let isSchedule = true;
   let mainContent;
@@ -125,28 +131,46 @@ const Home = ({ isLive, lang }) => {
       {/* Events COL */}
       {isSchedule ? <Schedule /> : null}
       {/* Main Col */}
-      <Col span={mainContent} style={{ flexGrow: 1, height: "100%", overflow: "auto" }}>
+      <Col
+        span={mainContent}
+        style={{ flexGrow: 1, height: "100%", overflow: "auto" }}
+      >
         {isLive ? <Stream /> : null}
-        <Row style={{ minHeight: '100%', padding: '1em' }}>
+        <Row style={{ minHeight: "100%", padding: "1em" }}>
           <Col span={19}>
             <Row gutter={[16, 0]}>
               {/* Aqui comienzan los posts */}
-              <StandardPost img={ExampleImg2} content={'¡Conoce al nuevo campeón de League of Legends!'} />
-              <StandardPost img={ExampleImg3} content={'¿Logrará Full Impact conquistar la copa?'} />
+              <StandardPost
+                img={ExampleImg2}
+                content={"¡Conoce al nuevo campeón de League of Legends!"}
+                type={"VIDEO"}
+              />
+              <StandardPost
+                img={ExampleImg3}
+                content={"¿Logrará Full Impact conquistar la copa?"}
+                type={"VIDEO"}
+              />
               <FullWidthPost />
               <BannerPromo />
-              <StandardPost img={ExampleImg} content={'Esto a penas comienza'} />
-              <StandardPost img={ExampleImg2} content={'¡Conoce al nuevo campeón de League of Legends!'} />
+              <StandardPost
+                img={ExampleImg}
+                content={"Esto a penas comienza"}
+                type={"ARTÍCULO"}
+              />
+              <StandardPost
+                img={ExampleImg2}
+                content={"¡Conoce al nuevo campeón de League of Legends!"}
+                type={"VIDEO"}
+              />
               <div className="load-more-container-outter">
                 <a className="load-more-container">
                   <h1>Cargar más</h1>
-                  <Icon.ChevronDown color={'white'} />
+                  <Icon.ChevronDown color={"white"} />
                 </a>
               </div>
-
             </Row>
           </Col>
-          <Col span={5} style={{ padding: '1em', paddingTop: 0 }}>
+          <Col span={5} style={{ padding: "1em", paddingTop: 0 }}>
             {/* Aqui comienza la publicidad */}
             <p className="ad-header">Publicidad</p>
             <Ad img={AdImage} />
@@ -155,7 +179,7 @@ const Home = ({ isLive, lang }) => {
         </Row>
         <PageEnd lang={lang} />
       </Col>
-    </Row >
+    </Row>
   );
 };
 export default Home;
